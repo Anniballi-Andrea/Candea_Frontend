ad820a54c420f8ce3c1c4cfe4e09e2c4792fe917
JuriPatacchiola <jpatacchiola7@gmail.com>
2026-01-09 17:03:36 +0100

    add card single

--- src/components/SingleCardProduct.jsx
+++ src/components/SingleCardProduct.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
 
-/*exoport default function SingleCardProduct({}){
-    return(
+export default function SingleCardProduct({ product }) {
+    const {
+        id = 1,
+        title = "Titolo Prodotto",
+        category = "Categoria",
+        price = "00.00",
+        imageUrl = "https://via.placeholder.com/300x400"
+    } = product || {};
+
+    return (
         <>
+            <div className="col-md-4 col-sm-6 mb-4">
+                <Link to={`/product/${id}`}>
+                    <div className="card h-100 border-0 shadow-sm position-relative overflow-hidden" style={{ backgroundColor: '#e0e0e0' }}>
+
+                        <div className="p-3">
+                            <img
+                                src={imageUrl}
+                                className="card-img-top object-fit-cover"
+                                alt={title}
+                                style={{ height: '250px', backgroundColor: '#fff' }}
+                            />
+                        </div>
+
+                        <div className="card-body d-flex flex-column justify-content-between p-4">
+                            <div>
+                                <h5 className="card-title fw-bold mb-1 text-dark">{title}</h5>
+                                <p className="card-text text-muted small uppercase">{category}</p>
+                            </div>
+
+                            <div className="mt-4 text-end">
+                                <span className="h4 fw-light text-secondary">
+                                    €{price}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
         </>
     )
-}*/
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import ProductList from "../components/ProductList";
+
 export default function Home() {
 
     return (
         <>
-
+            <ProductList />
         </>
     )
 }
