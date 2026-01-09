d38326f136907f60306b8fbff272f1bca193e425
angelostori <angelostori13@gmail.com>
2026-01-09 16:12:45 +0100

    add DefaultLayout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,7 +1,15 @@
-export default function DefaultLayout() {
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
+export default function DefaultLayout() {
     return (
         <>
+            <Header />
+            <main className="container">
+                <Outlet />
+            </main>
+            <Footer />
         </>
     )
 }
