0e04669ef96533da7320a8b6aee9e27d620acefe
JuriPatacchiola <jpatacchiola7@gmail.com>
2026-01-13 16:28:42 +0100

    add style for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,55 +2,43 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer className="bg-dark text-light py-4 mt-5">
-            <div className="container">
-                <div className="d-flex align-items-center justify-content-between gap-4">
+        <footer className="footer">
+            <div className="footer-container">
 
-                    <div>
-                        <h2 className="fw-bold">Candea</h2>
-                    </div>
-
-                    <ul className="nav justify-content-center gap-3">
-                        <li className="nav-item">
-                            <Link className="nav-link text-light" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link px-0 text-light" to="/">
-                                Products
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link px-0 text-light" to="/">
-                                About Us
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link px-0 text-light" to="/">
-                                Contacts
-                            </Link>
-                        </li>
-                    </ul>
-
-                    <div className="d-flex gap-3">
-                        <a href="#" className="text-light fs-4">
-                            <i className="bi bi-instagram"></i>
-                        </a>
-                        <a href="#" className="text-light fs-4">
-                            <i className="bi bi-facebook"></i>
-                        </a>
-                        <a href="#" className="text-light fs-4">
-                            <i className="bi bi-twitter-x"></i>
-                        </a>
-                    </div>
-
+                {/* LOGO */}
+                <div className="footer-logo">
+                    <Link to="/">
+                        <img src="/logo_scrittura_white.png" alt="Candea logo" />
+                    </Link>
                 </div>
 
-                <div className="text-center text-light mt-4">
-                    © {new Date().getFullYear()} Candea. All rights reserved.
+                {/* NAV */}
+                <ul className="footer-nav">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">Products</Link></li>
+                    <li><Link to="/">About Us</Link></li>
+                    <li><Link to="/">Contacts</Link></li>
+                </ul>
+
+                {/* SOCIAL */}
+                <div className="footer-social">
+                    <a href="#" aria-label="Instagram">
+                        <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="#" aria-label="Facebook">
+                        <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="#" aria-label="Twitter">
+                        <i className="bi bi-twitter-x"></i>
+                    </a>
                 </div>
+
+            </div>
+
+            {/* COPYRIGHT */}
+            <div className="footer-bottom">
+                © {new Date().getFullYear()} Candea. All rights reserved.
             </div>
         </footer>
-    )
+    );
 }
