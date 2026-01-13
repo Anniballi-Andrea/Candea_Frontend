2d70a4a71a4fbbfade9971b629173d345ddcaadb
Anniballi-Andrea <andreanniballi@gmail.com>
2026-01-13 16:59:39 +0100

    add a most sold section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,22 @@ import axios from "axios";
 
 export default function Home() {
     const [products, setProducts] = useState([]);
+    const [bestSellers, setBestSellers] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:3000/api/products?category")
             .then(response => { setProducts(response.data); })
             .catch(error => { console.error("Errore nel recupero dei prodotti:", error) })
+        axios.get("http://localhost:3000/api/products/bySold")
+            .then(response => { setBestSellers(response.data); })
+            .catch(error => { console.error("Errore nel recupero dei prodotti:", error) })
+
     }, []);
 
     return (
         <>
             <Bunner />
-            <ProductList products={products} />
+            <ProductList products={products} bestSellers={bestSellers} />
         </>
     )
 }
