96de80349912d87e34d6719eac5cd749c987e54a
angelostori <angelostori13@gmail.com>
2026-01-14 12:06:46 +0100

    update products title layout

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,10 +5,10 @@ import SingleCardProduct from "../components/SingleCardProduct";
 export default function Products() {
     const { products } = useContext(ProductsContext);
     return (
-        <div className="container my-5">
-            <h2 className="fw-bold mb-4">Lista prodotti</h2>
+        <div className="container p-5 text-center">
+            <h1>Lista prodotti</h1>
 
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+            <div className="my-3 row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
                 {products && products.map((prod, i) => (
                     <SingleCardProduct key={i} product={prod} />
                 ))}
