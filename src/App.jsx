72ce29f20bf8368efda8d93607f35d296b58f968
emilianocafasso <emiliano2.cafasso2@gmail.com>
2026-01-15 17:54:44 +0100

    add WishlistProvider and Wishlist page for managing user wishlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+import { WishlistProvider } from "./context/WishlistContext";
 import DefaultLayout from "./layout/DefaultLayout";
 import Cart from "./pages/cart";
+import Wishlist from "./pages/Wishlist"
 import CategoryProduct from "./pages/CategoryProduct";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
@@ -9,23 +11,27 @@ import Products from "./pages/Products";
 
 
 
+
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            {/* Definiamo la rotta con la 's' per i prodotti */}
-            <Route path="/products_view" element={<Products />} />
-            <Route path="/products/:slug" element={<ProductDetail />} />
-            <Route path="/products" element={<CategoryProduct />} />
-            <Route path="/products/category" element={<CategoryProduct />} />
-            <Route path="/cart" element={<Cart />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <WishlistProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Home />} />
+              {/* Definiamo la rotta con la 's' per i prodotti */}
+              <Route path="/products_view" element={<Products />} />
+              <Route path="/products/:slug" element={<ProductDetail />} />
+              <Route path="/products" element={<CategoryProduct />} />
+              <Route path="/products/category" element={<CategoryProduct />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </WishlistProvider>
   );
 }
 
