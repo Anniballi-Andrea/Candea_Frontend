26017a3de00191068182e917ccd42e7072706e6e
Anniballi-Andrea <andreanniballi@gmail.com>
2026-01-15 17:08:33 +0100

    fix the merge bugs

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import CheckoutForm from "../components/CheckoutForm";
 import { useCart } from "../context/CartContext";
-import { useState } from "react";
+
 import axios from "axios";
 
 export default function Cart() {
