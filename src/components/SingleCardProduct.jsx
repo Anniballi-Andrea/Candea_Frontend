a5f8fb42c344aeafbc1ec6ea8950e2d6f9d22712
JuriPatacchiola <jpatacchiola7@gmail.com>
2026-01-16 11:44:38 +0100

    change posiction and change dimension hart

--- src/components/SingleCardProduct.jsx
+++ src/components/SingleCardProduct.jsx
@@ -24,18 +24,19 @@ export default function SingleCardProduct({ product }) {
 		<div className="product-col">
 			<Link to={`/products/${product.slug}`} className="product-link">
 				<div className="product-card">
+					<i
+						className={`bi ${isLiked(product.id) ? "bi-suit-heart-fill" : "bi-suit-heart"}`}
+						onClick={(e) => {
+							e.preventDefault();
+							if (isLiked(product.id)) {
+								removeFromWishlist(product.id);
+							} else {
+								addToWishlist(product);
+							}
+						}}
+					></i>
 					<div className="product-image-wrapper">
-						<i
-							className={`bi ${isLiked(product.id) ? "bi-suit-heart-fill" : "bi-suit-heart"}`}
-							onClick={(e) => {
-								e.preventDefault();
-								if (isLiked(product.id)) {
-									removeFromWishlist(product.id);
-								} else {
-									addToWishlist(product);
-								}
-							}}
-						></i>
+
 						<img
 							src={`http://localhost:3000/${product.img}`}
 							alt={product.name}
