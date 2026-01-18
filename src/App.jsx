98d12453afaebbcc559ab44007c531b3a642c8a1
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-18 16:01:59 +0100

    remove productcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { CartProvider } from "./context/CartContext";
-import { ProductsProvider } from "./context/ProductContext";
 import { SearchProvider } from "./context/SearchContext";
 import { WishlistProvider } from "./context/WishlistContext";
 
@@ -18,33 +17,31 @@ import Wish from "./pages/Wish";
 
 function App() {
 	return (
-		<ProductsProvider>
-			<SearchProvider>
-				<WishlistProvider>
-					<CartProvider>
-						<BrowserRouter>
-							<Routes>
-								<Route element={<DefaultLayout />}>
-									<Route path="/" element={<Home />} />
-									{/* Definiamo la rotta con la 's' per i prodotti */}
-									<Route path="/products_view" element={<Products />} />
-									<Route path="/products/:slug" element={<ProductDetail />} />
-									<Route path="/products" element={<CategoryProduct />} />
-									<Route
-										path="/products/category"
-										element={<CategoryProduct />}
-									/>
-									<Route path="/cart" element={<Cart />} />
-									<Route path="/wishlist" element={<Wish />} />
-									<Route path="/summary_order" element={<SummaryOrder />} />
-									<Route path="*" element={<NotFound />} />
-								</Route>
-							</Routes>
-						</BrowserRouter>
-					</CartProvider>
-				</WishlistProvider>
-			</SearchProvider>
-		</ProductsProvider>
+		<SearchProvider>
+			<WishlistProvider>
+				<CartProvider>
+					<BrowserRouter>
+						<Routes>
+							<Route element={<DefaultLayout />}>
+								<Route path="/" element={<Home />} />
+								{/* Definiamo la rotta con la 's' per i prodotti */}
+								<Route path="/products_view" element={<Products />} />
+								<Route path="/products/:slug" element={<ProductDetail />} />
+								<Route path="/products" element={<CategoryProduct />} />
+								<Route
+									path="/products/category"
+									element={<CategoryProduct />}
+								/>
+								<Route path="/cart" element={<Cart />} />
+								<Route path="/wishlist" element={<Wish />} />
+								<Route path="/summary_order" element={<SummaryOrder />} />
+								<Route path="*" element={<NotFound />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</CartProvider>
+			</WishlistProvider>
+		</SearchProvider>
 	);
 }
 
