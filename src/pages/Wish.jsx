79a7b29e33c8ac0fb13e33e30a78747420aa480b
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-18 21:27:00 +0100

    cleanup wishlist

--- src/pages/Wish.jsx
+++ src/pages/Wish.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
+import ProductShowcase from "../components/ProductShowcase";
 import { useWishlist } from "../context/WishlistContext";
-import { useEffect } from "react";
-import SingleCardProduct from "../components/SingleCardProduct";
 
 export default function Wish() {
-	const { wishlist, removeFromWishlist } = useWishlist();
+	const { wishlist } = useWishlist();
 
 	return (
 		<div className="container my-5 py-5">
 			<h2 className="mb-5 fw-bold text-center">LA TUA WISHLIST</h2>
-			{wishlist.lenght === 0 ? (
+			{wishlist.length === 0 ? (
 				<div className="text-center py-5 border rounded bg-light">
 					<p className="fs-4 text-muted">La tua wishlist è vuota</p>
 					<Link to="/" className="btn btn-dark px-4 mt-3">
@@ -17,14 +16,7 @@ export default function Wish() {
 					</Link>
 				</div>
 			) : (
-				<div className="my-3 row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
-					{wishlist.map((product, index) => (
-						<SingleCardProduct
-							key={index}
-							product={product}
-						></SingleCardProduct>
-					))}
-				</div>
+				<ProductShowcase title={""} products={wishlist} />
 			)}
 		</div>
 	);
