26ba48ee88aa44e6de9ac28ec25b60012d7b2e35
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-18 21:13:35 +0100

    add container and link to homepage in order summary

--- src/pages/SummaryOrder.jsx
+++ src/pages/SummaryOrder.jsx
@@ -1,69 +1,108 @@
-
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 
-
 export default function SummaryOrder() {
-    const { orderData } = useCart();
-    const { total, cart, city, email, first_name, last_name, phone_number, province, street, street_number, zip_code } = orderData
+	const { orderData } = useCart();
+	const {
+		total,
+		cart,
+		city,
+		email,
+		first_name,
+		last_name,
+		phone_number,
+		province,
+		street,
+		street_number,
+		zip_code,
+	} = orderData;
 
-    const [spedition, setSpedition] = useState(false)
+	// const [shipping, setShipping] = useState(false);
 
-    function SetSpeditionCost(cart) {
-        let totalPrice = 0
-        cart.forEach(el => {
-            totalPrice += Number(el.actual_price) * el.quantity
-            console.log(totalPrice)
-        })
-        if (totalPrice <= 90) {
-            setSpedition(true)
-        }
-    }
+	// function setShippingCost(cart) {
+	// 	let totalPrice = 0;
+	// 	cart.forEach((el) => {
+	// 		totalPrice += Number(el.actual_price) * el.quantity;
+	// 		console.log(totalPrice);
+	// 	});
+	// 	if (totalPrice <= 90) {
+	// 		setShipping(true);
+	// 	}
+	// }
 
-    useEffect(() => { SetSpeditionCost(cart), console.log(spedition) }, [])
+	// useEffect(() => {
+	// 	setShippingCost(cart);
+	// }, []);
 
-    return (
+	return (
+		<div className="container mt-5">
+			<div className="card border-0 bg-light p-4 shadow-sm">
+				<h4 className="fw-bold mb-4 text-success">
+					Ordine avvenuto con successo
+				</h4>
 
-        <div className="card border-0 bg-light p-4 shadow-sm">
-            <h4 className="fw-bold mb-4 text-success">Ordine avvenuto con successo</h4>
-
-            {/* Lista prodotti stile scontrino */}
-            <div className="cart-summary-list mb-3">
-                {cart.map((item) => (
-                    <>
-                        <div
-                            key={item.id}
-                            className="d-flex justify-content-between align-items-center mb-2"
-                        >
-                            <div style={{ wordBreak: "break-word" }}>
-                                <span className="fw-bold">{item.name}</span>
-                                <span className="text-muted ms-2">x{item.quantity}</span>
-                            </div>
-                            <div>
-                                €{(Number(item.actual_price) * item.quantity).toFixed(2)}
-                            </div>
-
-                        </div>
-                        <hr />
-                    </>
-                ))}
-                <div className="d-flex justify-content-end">
-                    <div>Totale: €{total.toFixed(2)}</div>
-                </div>
-
-
-            </div>
-            <div>
-                <h2>Acquisto effettutato da:</h2>
-                <p>Nome: {first_name}</p>
-                <p>Cognome: {last_name}</p>
-                <p>email: {email}</p>
-                <p>numero di telefono: {phone_number}</p>
-                <p>Città: {city}</p>
-                <p>Povincia: {province}</p>
-                <p>Via: {street} {street_number}</p>
-                <p>CAP: {zip_code}</p>
-            </div>
-        </div>
-    )
+				{/* Lista prodotti stile scontrino */}
+				<div className="cart-summary-list mb-3">
+					{cart.map((item) => (
+						<>
+							<div
+								key={item.id}
+								className="d-flex justify-content-between align-items-center mb-2"
+							>
+								<div style={{ wordBreak: "break-word" }}>
+									<span className="fw-bold">{item.name}</span>
+									<span className="text-muted ms-2">x{item.quantity}</span>
+								</div>
+								<div>
+									€{(Number(item.actual_price) * item.quantity).toFixed(2)}
+								</div>
+							</div>
+							<hr />
+						</>
+					))}
+					<div className="d-flex justify-content-end">
+						<div>
+							Totale: <strong>€{total.toFixed(2)}</strong>
+						</div>
+					</div>
+				</div>
+				<div>
+					<h4>Acquisto effettutato da:</h4>
+					<p>
+						Nome: <strong>{first_name}</strong>
+					</p>
+					<p>
+						Cognome: <strong>{last_name}</strong>
+					</p>
+					<p>
+						email: <strong>{email}</strong>
+					</p>
+					<p>
+						numero di telefono: <strong>{phone_number}</strong>
+					</p>
+					<p>
+						Città: <strong>{city}</strong>
+					</p>
+					<p>
+						Povincia: <strong>{province}</strong>
+					</p>
+					<p>
+						Via:{" "}
+						<strong>
+							{street} {street_number}
+						</strong>
+					</p>
+					<p>
+						CAP: <strong>{zip_code}</strong>
+					</p>
+				</div>
+			</div>
+			<div className="d-flex justify-content-center">
+				<Link to="/" className="btn btn-dark px-4 mt-3 ml-auto">
+					Torna alla homepage
+				</Link>
+			</div>
+		</div>
+	);
 }
